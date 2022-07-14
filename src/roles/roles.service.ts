08892cca916b10823003407662f030dfed14e1a8
Nikolay <Chernuh.1994@gmail.com>
2022-07-14 18:37:06 +0300

    Added getUserRoleOrCreate method.

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Role } from './entities/role.entity';
 import { CreateRoleDto } from './dto/create-role.dto';
-import { RoleType } from './types/roles.type';
+import { RoleType, RoleValues } from './types/roles.type';
 
 @Injectable()
 export class RolesService {
@@ -19,5 +19,20 @@ export class RolesService {
   async create(createRoleDto: CreateRoleDto): Promise<Role> {
     const newRole = this.roleRepository.create(createRoleDto);
     return this.roleRepository.save(newRole);
+  }
+
+  async getUserRoleOrCreate(): Promise<Role> {
+    let userRole = await this.findOne(RoleValues.USER);
+
+    if (!userRole) {
+      const createRoleDto: CreateRoleDto = {
+        name: RoleValues.USER,
+        description: 'Role for unverified user',
+      };
+
+      userRole = await this.create(createRoleDto);
+    }
+
+    return userRole;
   }
 }
