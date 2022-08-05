956a4f16e21185dc726ca27430c929838219a7d8
Nikolay <Chernuh.1994@gmail.com>
2022-08-05 22:00:28 +0300

    Fixed module "roles". Invalid "RoleEntity" import.

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -2,7 +2,7 @@ import { forwardRef, Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesService } from './roles.service';
-import { Role } from './entities/role.entity';
+import { RoleEntity } from './entities/role.entity';
 import { RolesGuard } from './guards/roles.guard';
 import { UsersModule } from '../users/users.module';
 
@@ -14,7 +14,7 @@ import { UsersModule } from '../users/users.module';
     },
     RolesService,
   ],
-  imports: [forwardRef(() => UsersModule), TypeOrmModule.forFeature([Role])],
+  imports: [forwardRef(() => UsersModule), TypeOrmModule.forFeature([RoleEntity])],
   exports: [RolesService],
 })
 export class RolesModule {}
