213d73dacd729291d8092fdfa7f893a48086d8c1
Nikolay <Chernuh.1994@gmail.com>
2022-08-05 22:00:10 +0300

    fixed module "products". Invalid "ProductEntity" import.

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-import { Product } from './entities/product.entity';
+import { ProductEntity } from './entities/product.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
 })
 export class ProductsModule {}
