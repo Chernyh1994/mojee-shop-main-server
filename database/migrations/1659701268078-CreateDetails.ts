1d88173b3034b84d34a0f24055fc3b44c0185255
Nikolay <Chernuh.1994@gmail.com>
2022-08-05 17:18:58 +0300

    Added migration create details table.

---
+++ database/migrations/1659701268078-CreateDetails.ts
@@ -0,0 +1,72 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateDetails1659701268078 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'details',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'full_name',
+            type: 'varchar',
+            length: '100',
+            isNullable: false,
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            length: '300',
+            isNullable: true,
+          },
+          {
+            name: 'size',
+            type: 'enum',
+            enum: ['XS', 'S', 'M', 'L', 'XL'],
+            enumName: 'details_size_enum[]',
+            isNullable: false,
+          },
+          {
+            name: 'fabric_type',
+            type: 'varchar',
+            length: '50',
+            isNullable: false,
+          },
+          {
+            name: 'product_country',
+            type: 'varchar',
+            length: '50',
+            isNullable: false,
+          },
+          {
+            name: 'brand',
+            type: 'varchar',
+            length: '50',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('details');
+  }
+}
