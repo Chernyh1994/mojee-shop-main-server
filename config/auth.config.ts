71230d16765b162df0cf3ea3f71ab8cf8d246929
Nikolay <Chernuh.1994@gmail.com>
2022-08-08 19:05:19 +0300

    Renamed env var from JWT_SECRET to JWT_ACCESS_SECRET.

--- config/auth.config.ts
+++ config/auth.config.ts
@@ -1,6 +1,5 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('auth', () => ({
-  jwtSecret: process.env.JWT_SECRET,
-  expires: 600000,
+  jwtSecret: process.env.JWT_ACCESS_SECRET,
 }));
