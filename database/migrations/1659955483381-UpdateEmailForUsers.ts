88ac73b1fcac7501f4908e207cf6d43383a37808
Nikolay <Chernuh.1994@gmail.com>
2022-08-08 17:42:28 +0300

    Added unique email field for users table.

---
+++ database/migrations/1659955483381-UpdateEmailForUsers.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class UpdateEmailForUsers1659955483381 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'users',
+      'email',
+      new TableColumn({
+        name: 'email',
+        type: 'varchar',
+        length: '75',
+        isUnique: true,
+        isNullable: false,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'users',
+      'email',
+      new TableColumn({
+        name: 'email',
+        type: 'varchar',
+        length: '75',
+        isNullable: false,
+      }),
+    );
+  }
+}
