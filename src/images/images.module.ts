f2f119efae38b4de522fb0c22dfc2d8c7bef09e6
Nikolay <Chernuh.1994@gmail.com>
2022-08-10 21:23:27 +0300

    Updated images module.

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ImagesController } from './images.controller';
 import { ImagesService } from './images.service';
 import { ImageEntity } from './entities/image.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ImageEntity])],
-  controllers: [ImagesController],
+  controllers: [],
   providers: [ImagesService],
 })
 export class ImagesModule {}
